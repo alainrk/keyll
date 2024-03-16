7b8409911afd1bfc687f16f06a2b2220c5bdd97e
Alain <alain.devcs@gmail.com>
2024-03-16 05:39:54 +0100

    score counters improved

--- script.js
+++ script.js
@@ -7,7 +7,11 @@ const CHARSET_UPPERCASE = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
 const CHARSET_NUMBERS = `0123456789`;
 const CHARSET_SPECIAL = `!@#$%^&*()_+~-=\`{}[]|:;'"<>,.?/\\`;
 
-const CHARSET = new Set(`${CHARSET_LOWERCASE}${CHARSET_UPPERCASE}${CHARSET_NUMBERS}${CHARSET_SPECIAL}`.split(''));
+const CHARSET = new Set(
+  `${CHARSET_LOWERCASE}${CHARSET_UPPERCASE}${CHARSET_NUMBERS}${CHARSET_SPECIAL}`.split(
+    ""
+  )
+);
 
 // CharsQueue just stores a fixed amount of typed characters
 class CharsQueue {
@@ -23,7 +27,7 @@ class CharsQueue {
   }
 
   addChar(char, hit, upcoming = false) {
-    let popped
+    let popped;
 
     if (this.queue.length > 0) {
       this.queue[this.queue.length - 1].current = false;
@@ -33,14 +37,14 @@ class CharsQueue {
       char,
       hit,
       upcoming,
-      current: true
+      current: true,
     });
 
     if (this.queue.length > this.maxLength) {
       popped = this.queue.shift();
     }
 
-    return popped
+    return popped;
   }
 
   getString() {
@@ -48,14 +52,15 @@ class CharsQueue {
   }
 
   getLastChar() {
-    return this.queue[this.queue.length - 1].char;
+    return this.queue[this.queue.length - 1];
   }
 }
 
 class Game {
   constructor() {
     // TODO: Add a proper options object param
-    this.score = 0;
+    this.hitCount = 0;
+    this.totCount = 0;
     this.timeout = 5;
     this.missedChar = false;
     this.expire = 0;
@@ -64,27 +69,39 @@ class Game {
     this.enableNumbers = false;
     this.enableSpecial = false;
     this.availableChars = this.createAvailableChars();
-    this.charset = new Set(this.availableChars.split(''));
+    this.charset = new Set(this.availableChars.split(""));
     this.typedCharsQueue = new CharsQueue(45);
     this.promptedCharsQueue = new CharsQueue(11);
     this.upcomingCharsQueue = new CharsQueue(10);
 
     // Preallocate upcoming chars
     for (let i = 0; i < 10; i++) {
-      this.upcomingCharsQueue.addChar(this.getRandomCharFromCharSet(), false, true);
+      this.upcomingCharsQueue.addChar(
+        this.getRandomCharFromCharSet(),
+        false,
+        true
+      );
     }
     this.updateChar();
     this.resetTimeout();
   }
 
   get currentChar() {
-    return this.promptedCharsQueue.getLastChar();
+    return this.promptedCharsQueue.getLastChar().char;
+  }
+
+  get currentCharIsHit() {
+    return this.promptedCharsQueue.getLastChar().hit;
   }
 
   updateChar() {
-    const popped = this.upcomingCharsQueue.addChar(this.getRandomCharFromCharSet(), false, true);
-    // this.promptedCharsQueue.addChar(this.getRandomCharFromCharSet(), true);
+    const popped = this.upcomingCharsQueue.addChar(
+      this.getRandomCharFromCharSet(),
+      false,
+      true
+    );
     this.promptedCharsQueue.addChar(popped.char, true);
+    this.totCount++;
   }
 
   resetTimeout() {
@@ -93,13 +110,15 @@ class Game {
 
   handleTypedChar(char) {
     if (key === game.currentChar) {
-      this.score++;
+      if (this.currentCharIsHit) {
+        this.hitCount++;
+      }
       this.missedChar = false;
       this.updateChar();
       this.resetTimeout();
       this.typedCharsQueue.addChar(char, true);
       this.promptedCharsQueue.updateCurrent(true);
-      return
+      return;
     }
 
     this.missedChar = true;
@@ -132,7 +151,9 @@ class Game {
   }
 
   getRandomCharFromCharSet() {
-    return this.availableChars[Math.floor(Math.random() * this.availableChars.length)];
+    return this.availableChars[
+      Math.floor(Math.random() * this.availableChars.length)
+    ];
   }
 }
 
@@ -172,7 +193,7 @@ function setup() {
   noStroke();
   background(0);
   // A mono is better
-  textFont('Courier New');
+  textFont("Courier New");
   // Reduce framerate
   frameRate(10);
   rectWidth = width / 4;
@@ -180,19 +201,19 @@ function setup() {
 }
 
 function draw() {
-  let textChunks = []
+  let textChunks = [];
 
   // Clear the screen
   background(0);
 
-  // Write the score at the top left
+  // Write the hitCount at the top left
   fill(255);
-  textSize(32);
-  text("Score: " + game.score, 20, 40);
+  textSize(25);
+  text(`Score: ${game.hitCount}/${game.totCount}`, 20, 40);
 
   // Write the already typed chars
   textSize(50);
-  textChunks = []
+  textChunks = [];
   for (const chunk of game.promptedCharsQueue.queue) {
     // Skip the current
     if (chunk.current) {
@@ -212,12 +233,16 @@ function draw() {
 
   textSize(100);
   textStyle(BOLD);
-  text(game.currentChar, width / 2 - textWidth(game.currentChar) / 2, height / 2 + textWidth(game.currentChar) / 3);
+  text(
+    game.currentChar,
+    width / 2 - textWidth(game.currentChar) / 2,
+    height / 2 + textWidth(game.currentChar) / 3
+  );
   textStyle(NORMAL);
 
   // Write the upcoming chars
   textSize(50);
-  textChunks = []
+  textChunks = [];
   for (const chunk of game.upcomingCharsQueue.queue) {
     const color = chunk.hit ? [250, 250, 250, 100] : [250, 250, 250, 100];
     textChunks.push([chunk.char, color]);
@@ -226,7 +251,7 @@ function draw() {
 
   // Write the typedCharsQueue at the bottom
   textSize(25);
-  textChunks = []
+  textChunks = [];
   for (const chunk of game.typedCharsQueue.queue) {
     const color = chunk.hit ? [0, 200, 0] : [200, 0, 0];
     textChunks.push([chunk.char, color]);
@@ -236,5 +261,6 @@ function draw() {
 }
 
 function keyPressed() {
+  console.log(keyCode, key);
   game.handleTypedChar(key);
 }
