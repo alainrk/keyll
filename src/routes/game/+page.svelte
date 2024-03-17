0e22cc2ab44f9a9e4d6b25ba52e7dbca041f6417
Alain <alain.devcs@gmail.com>
2024-03-17 08:16:46 +0100

    including p5 in the project

---
+++ src/routes/game/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import P5, { type Sketch } from 'p5-svelte';
+
+	const sketch: Sketch = (p5) => {
+		p5.setup = () => {
+			p5.createCanvas(p5.windowWidth, p5.windowHeight);
+		};
+		p5.draw = () => {
+			p5.ellipse(p5.width / 2, p5.height / 2, p5.width, p5.height);
+		};
+	};
+</script>
+
+<P5 {sketch} />
